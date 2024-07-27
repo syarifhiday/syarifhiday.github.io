6a1630cbd5ef852ae1fbf09701a7deaaac29c9c1
Syarif Hidayatulloh <iamsyarifh@gmail.com>
2024-07-27 21:51:52 +0700

    edit porto

--- app.js
+++ app.js
@@ -1,35 +1,57 @@
 
 projects = [
-    {
-        title: "IoT clothesline system controller application",
-        image: "images/porto1.webp",
-        tech: "Android Kotlin",
-        link: "https://github.com/syarifhiday/Aplikasi-Sistem-Jemuran-IoT"
-    },
-    {
-        title: "IoT coffee machine controller application",
-        image: "images/porto2.webp",
-        tech: "Android Kotlin",
-        link: "https://github.com/syarifhiday/Aplikasi-Asisten-Kopi"
-    },
-    {
-        title: "FaceCare - An App to Help Maintain Your Facial Health",
-        image: "images/porto3.webp",
-        tech: "Android Kotlin",
-        link: "https://github.com/syarifhiday/FaceCare"
-    },
-    {
-        title: "Nadagitar Store - Wordpress eCommerce",
-        image: "images/porto4.webp",
-        tech: "Wordpress",
-        link: "https://shop.nadagitar.net"
-    },
-    {
-        title: "Sales Insights: Analyzing Kaggle's Sales Data",
-        image: "images/porto5.webp",
-        tech: "Python, Looker Studio",
-        link: "https://medium.com/@iamsyarifh/proyek-analisis-data-penjualan-dan-pelanggan-34e1ee2c45ec"
-    }
+    android= [
+        {
+            title: "IoT clothesline system controller application",
+            image: "images/porto1.webp",
+            tech: "Android Kotlin",
+            link: "https://github.com/syarifhiday/Aplikasi-Sistem-Jemuran-IoT"
+        },
+        {
+            title: "IoT coffee machine controller application",
+            image: "images/porto2.webp",
+            tech: "Android Kotlin",
+            link: "https://github.com/syarifhiday/Aplikasi-Asisten-Kopi"
+        },
+        {
+            title: "FaceCare - An App to Help Maintain Your Facial Health",
+            image: "images/porto3.webp",
+            tech: "Android Kotlin",
+            link: "https://github.com/syarifhiday/FaceCare"
+        }
+        
+    ],
+    web= [
+        {
+            title: "Nadagitar Store - Wordpress eCommerce",
+            image: "images/porto4.webp",
+            tech: "Wordpress",
+            link: "https://shop.nadagitar.net"
+        }
+    ],
+    cysec= [
+        {
+            image: "images/cert/9.webp"
+        },
+        {
+            image: "images/cert/10.webp"
+        },
+        {
+            image: "images/cert/11.webp"
+        },
+        {
+            image: "images/cert/12.webp"
+        }
+
+    ],
+    data= [
+        {
+            title: "Sales Insights: Analyzing Kaggle's Sales Data",
+            image: "images/porto5.webp",
+            tech: "Python, Looker Studio",
+            link: "https://medium.com/@iamsyarifh/proyek-analisis-data-penjualan-dan-pelanggan-34e1ee2c45ec"
+        }
+    ]
 ]
 
 certificates = [
@@ -37,19 +59,10 @@ certificates = [
         image: "images/cert/0.webp"
     },
     {
-        image: "images/cert/1.webp"
-    },
-    {
-        image: "images/cert/2.webp"
-    },
-    {
         image: "images/cert/3.webp"
     },
     {
         image: "images/cert/4.webp"
-    },
-    {
-        image: "images/cert/5.webp"
     },
     {
         image: "images/cert/6.webp"
